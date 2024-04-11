6c68ee321845a73c4a14bd6e17727fcb06195a29
RahulBharadia09 <rahulbharadia917@gmail.com>
2024-04-11 13:12:45 +0530

    Added Variables and data types

---
+++ DataTypes.py
@@ -0,0 +1,20 @@
+"""
+Five Types of primary data types in python 
+    1] Integer (int)
+    2] String   (str)
+    3]Float (float)
+    4]Boolean (bool)
+    5]None  (NoneType
+    )
+"""
+age = 23
+name = "Rahul"
+height = 5.5
+old = False
+Address = None
+
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(old))
+print(type(Address))
---
+++ variables.py
@@ -0,0 +1,21 @@
+# what is variable (Identifier) ? it is a name given to the memory location in python
+"""
+Rules of variables (Identifier)  : 
+    can be a combination of uppercase lowercase digits and the underscore 
+    identifier cannot start with digits 
+    cannot use special symbols like !,@.%,$ etc
+    it can be of any length.                                        
+"""
+
+name = "Coder"
+name2 = "Vipul"
+name_3= "Ajeet"
+age = 22
+print(name,name2,name_3)
+
+# _________________________________________________________________________________________________________________
+
+# To know the type of the identifier we can use type() function
+
+print(type(name2))
+print(type(age))
