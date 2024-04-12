0a47e8f05267a4d0e8db7b40d6b9ec66ebd52967
RahulBharadia09 <rahulbharadia917@gmail.com>
2024-04-13 00:32:24 +0530

    Added str function file

---
+++ StrFunctions.py
@@ -0,0 +1,18 @@
+#  Basic str functions in the python 
+#  check documentation for the more function details
+
+
+str = 'i am a coder'
+
+print(str.endswith('d')) #false
+
+print(str.capitalize()) 
+
+print(str.replace("coder" , "Rahul"))
+print(str)
+
+print(str.find("coder")) #returns the index number
+
+print(str.count('e'))
+
+
